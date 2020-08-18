f76958b11817f07a301a56570f34f538573236f1
Alex <shura.oblizin@mail.ru>
2020-08-18 06:26:18 +0300

    поправил - убрал дублирующие результаты

--- src/ArrSumOfTwoElems.java
+++ src/ArrSumOfTwoElems.java
@@ -27,7 +27,7 @@ public class ArrSumOfTwoElems {
     public static void twoSum(int[] array, int inp) {
         System.out.println("Сумма равная " + inp + " получается из элементов с индексами: ");
         for (int i = 0; i < array.length; i++) {
-            for (int k = 0; k < array.length; k++) {
+            for (int k = i; k < array.length; k++) {
                 if (array[i] + array[k] == inp && i != k) {
                     System.out.print("[" + i + "," + k + "]" + " ");
                 }
