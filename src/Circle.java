3681ad6ab0d4208fa447cc790a374e11675b5408
Andrey Litvinov <litwinoff@gmail.com>
2020-08-24 14:47:58 +0300

    Замечания по задаче 'Иерархия'

--- src/Circle.java
+++ src/Circle.java
@@ -1,19 +1,24 @@
 public class Circle extends Point{
-    static int id = 2;
-    static int count;
+    //    static int id = 2;
+//    static int count;
     double rad;
+    final String type = "Круг";
 
     Circle(int x, int y, double rad){
         super(x,y);
         this.rad = rad;
 
     }
-    public int getIdAndCount(){
-        count++;
-        System.out.println("Создан объект: " + count);
-        System.out.print("Геометрическая фигура: ");
-        GeometryLoader.nameFigure(id);
-        return id;
+//    public int getIdAndCount(){
+//        count++;
+//        System.out.println("Создан объект: " + count);
+//        System.out.print("Геометрическая фигура: ");
+//        GeometryLoader.nameFigure(id);
+//        return id;
+//
+//    }
 
+    public String getType() {
+        return type;
     }
 }
