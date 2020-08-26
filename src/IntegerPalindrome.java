258f34549fe3f25ab6b175d7c12468cbf55afcb6
Alex <shura.oblizin@mail.ru>
2020-08-26 12:08:20 +0300

    проверка числа на палиндром

---
+++ src/IntegerPalindrome.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class IntegerPalindrome {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Число палиндром? - " + ArrayInt.isPalindrome(ArrayInt.arrFilled()));
+        }
+    }
+}
+
+class ArrayInt {
+
+    public static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nВведите число: ");
+        int number = scanner.nextInt();
+
+        return number;
+    }
+
+    public static int[] array(int input) {
+        int count = 0;
+        while (input > 0) {
+            input = input / 10;
+            count++;
+        }
+        return new int[count];
+    }
+
+    public static int[] arrFilled() {
+        int i;
+        int tmp = getInt();
+        int[] arrayTmp = array(tmp);
+        for (i = 0; i < arrayTmp.length; i++) {
+            arrayTmp[i] = tmp % 10;
+            tmp /= 10;
+        }
+
+        return arrayTmp;
+    }
+
+    public static boolean isPalindrome(int[] arr) {
+        boolean ispal = false;
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1]) break;
+            else {
+                ispal = true;
+                continue;
+            }
+        }
+
+        return ispal;
+    }
+}
