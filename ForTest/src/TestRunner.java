bd6b6f0dfe355e51c4dfa3924a7198651ed9e580
Alex <shura.oblizin@mail.ru>
2020-08-26 14:59:11 +0300

    test вроде исправил

--- ForTest/src/TestRunner.java
+++ ForTest/src/TestRunner.java
@@ -6,7 +6,7 @@ class TestRunner {
     void sum() {
         Test underTest = new Test();
         int actual = underTest.sum(-121,10);
-        int expected = underTest.sum(10,-121);
-        assertEquals(actual,expected);
+
+        assertEquals(-111,actual);
     }
 }
